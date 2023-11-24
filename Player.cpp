0b9c32f1f147eca43c353e7f85bae04b13cd5627
arorah7 <arorah7@mcmaster.ca>
2023-11-23 20:29:58 -0500

    Added Player Class

--- Player.cpp
+++ Player.cpp
@@ -7,8 +7,8 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos.setObjPos(5,5,'@');
 }
-
 
 Player::~Player()
 {
@@ -17,16 +17,63 @@ Player::~Player()
 
 void Player::getPlayerPos(objPos &returnPos)
 {
+    returnPos.setObjPos(playerPos.x, playerPos.y, playerPos.symbol);
     // return the reference to the playerPos arrray list
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic
+    switch(mainGameMechsRef->getInput()){
+        case 'w':
+            if(myDir != UP && myDir != DOWN)
+                myDir = UP;
+            break;
+
+        case 'a':
+            if(myDir != LEFT && myDir != RIGHT)
+                myDir = LEFT;
+            break;
+
+        case 's':
+            if(myDir != UP && myDir != DOWN)
+                myDir = DOWN;
+            break;
+
+        case 'd':
+            if(myDir != LEFT && myDir != RIGHT)
+                myDir = RIGHT;
+            break;
+    }        
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    switch(myDir){
+        case(UP):
+            playerPos.y--;
+            if(playerPos.y <= 0) // Wraparound logic
+                playerPos.y = mainGameMechsRef->getBoardSizeY() - 2;
+            break;
+
+        case(DOWN):
+            playerPos.y++;
+            if(playerPos.y >= mainGameMechsRef->getBoardSizeY() - 1); // Wraparound logic
+                playerPos.y = 1;
+            break;
+
+        case(LEFT):
+            playerPos.x--;
+            if(playerPos.x <= 0) // Wraparound logic
+                playerPos.x = mainGameMechsRef->getBoardSizeX() - 2;
+            break;
+
+        case(RIGHT):
+            playerPos.x++;
+            if(playerPos.y >= mainGameMechsRef->getBoardSizeX() - 1) // Wraparound logic
+                playerPos.y = 1;
+            break;
+    }
 }
 
