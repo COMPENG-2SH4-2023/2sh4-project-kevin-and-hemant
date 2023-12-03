bd7eac88b420e64a23bd52dbb10bc7434b91c2c5
Kevin Adieb <adiebk@mcmaster.ca>
2023-12-03 18:59:26 -0500

    DONE

--- Project.cpp
+++ Project.cpp
@@ -144,8 +144,6 @@ void DrawScreen(void)
     //messages printed under the game board to explain special foods, cheats, board size, player position, score and length of snake
     MacUILib_printf("There are 2 special foods in disguise, \nOne makes you shorter by 2 units, \nThe other adds 10 to score!\n\n");
     MacUILib_printf("Press c to show where the special foods are\n");
-
-    MacUILib_printf("Board size: %dx%d, Player Position: <%d,%d> + %c\n\n", myGM->getBoardSizeX(), myGM->getBoardSizeY(), tempBodySeg.x, tempBodySeg.y, tempBodySeg.symbol);
     
     MacUILib_printf("Score: %d\n", myGM->getScore());
     MacUILib_printf("Snake length: %d\n", playerBody->getSize());
